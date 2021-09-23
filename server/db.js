2a0de33c865fecd9dafb82a86a7f55891af93e3a
Tyler Boright <tyler@tylerboright.com>
2021-09-23 17:09:02 +1000

    updates the table creation code to create related tables needed for the table matches

---
+++ server/db.js
@@ -0,0 +1,88 @@
+const process = require('process');
+
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database("leaderboard.db3", (err) => {
+    if (err) {
+        console.error('could not connect to the database withe the following err: ', err);
+        console.error('please check your database connection and try again');
+        throw err;
+    } else {
+        console.log(`connected to database`);
+    }
+});
+
+const createPlayersStr = `CREATE TABLE IF NOT EXISTS players (
+        id integer PRIMARY KEY,
+        name TEXT,
+        wins INT,
+        losses INT,
+        gamesPlayed INT,
+        pointsPerGame REAL
+    )`;
+
+const createMatchesStr = `CREATE TABLE IF NOT EXISTS matches (
+        id integer PRIMARY KEY,
+        matchTime TEXT,
+        location TEXT
+    )`;
+
+const createPlayerMatchesStr = `CREATE TABLE IF NOT EXISTS player_matches (
+        player_id INT,
+        match_id INT,
+        finalGameScore INT,
+        didWin INT,
+        FOREIGN KEY(player_id) REFERENCES players(id) ON DELETE CASCADE,
+        FOREIGN KEY(match_id) REFERENCES matches(id) ON DELETE CASCADE
+    )`;
+
+const createStrings = [createPlayersStr, createMatchesStr, createPlayerMatchesStr];
+
+const closeDb = (code) => { 
+    db.close() 
+    process.exit(code);
+};
+
+// add stdin resume so that we can catch the signals on Ctrl (Cmd) + c and other kill commands.
+process.stdin.resume();
+process.on('exit', closeDb);
+process.on('SIGINT', closeDb);
+process.on('SIGTERM', closeDb);
+
+module.exports = {
+    connect: () => {
+        return new Promise((resolve, reject) => {
+            db.serialize((serialErr) => {
+                for (let createString of createStrings) {
+                    db.run(createString, (err) => {
+                        if (err) {
+                            console.error('Error creating table in the database: ', err)
+                            reject(err);
+                        } 
+                    });
+                }
+
+                if (serialErr) {
+                    console.error('Error in serializing table creation: ', err);
+                    reject(err);
+                }
+
+                resolve();
+            });               
+        });
+    },
+
+    run: (sql, params = []) => {
+        return new Promise((resolve, reject) => {
+            db.run(sql, params, (err) => {
+                if (err) {
+                    console.error('Error running the following sql: ', sql);
+                    console.error(err);
+                    reject(err);
+                } else {
+                    resolve("OK");
+                }
+            });
+        });
+    }
+}; 
--- server/index.js
+++ server/index.js
@@ -1,18 +1,31 @@
 const express = require('express');
+const db = require('./db');
 const utils = require('./utils');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const createApiRoute = (version) => 
+db
+    .connect()
+    .then(_res => console.log('table created successfully'))
+    .catch(err => {
+        console.error('the db was unable to connect with the following error: \n', err);
+        // throwing uncaught error to force the process to exit instead of calling process.exit(1);
+        // pattern defined in documentation: https://nodejs.org/api/process.html#process_process_exit_code
+        throw err;
+    });
 
 app.use((req, res, next) => {
     res.setHeader("Content-Type", "application/json");
     next();
 });
 
-app.get(utils.createV1ApiString("/"), (req, res) => {
+app.get(utils.createV1ApiString("/players"), (req, res) => {
+    res.json({message: "Getting players"});
+});
+
+app.post(utils.createV1ApiString("/matches"), (req, res) => {
     res.json({message: "Hello from server!"});
 });
 
