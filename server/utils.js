b88802568e6ddc15ab1bafc60b864f16188ee1c7
Tyler Boright <tyler@tylerboright.com>
2021-09-24 13:52:49 +1000

    removes the unused api string creation function that was unnecessary after using graphql; adds an update function that 1. adds a dynamic rating system based on the elo rating system used for many sports and 2. updates the losing party and winning party respectively

--- server/utils.js
+++ server/utils.js
@@ -1,5 +1,20 @@
-const createApiAppendedString = (version) => (string) => `/api${version}${string}`;
+const updatePlayerStats = (playerObject, opponentRating, didWin) => {
+    if (Object.entries(playerObject).length == 0 || typeof opponentRating === 'undefined' || typeof didWin === 'undefined') {
+        throw new TypeError("playerObject and didWin params are both required for the updatePlayerStatsFunction");
+    }
 
-const createV1ApiString = createApiAppendedString("/v1");
+    playerObject.opponentsRatings += opponentRating;
+    playerObject.gamesPlayed += 1;
+    if (didWin) {
+        playerObject.wins += 1;
+    } else {
+        playerObject.losses += 1;
+    }
 
-module.exports = {createV1ApiString};
+    // simplified elo-type rating system (performance rating system found here https://en.wikipedia.org/wiki/Elo_rating_system#Implementing_Elo's_scheme) to better rank players based on the ranks of other players they have defeated before
+    playerObject.rating = Math.floor((playerObject.opponentsRatings + 400 * (playerObject.wins - playerObject.losses)) / playerObject.gamesPlayed);
+
+    return playerObject;
+}
+
+module.exports = {updatePlayerStats};
