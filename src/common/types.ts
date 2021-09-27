f21faa8f72d9c230380e836e92495cd20c05a33c
Tyler Boright <tyler@tylerboright.com>
2021-09-27 22:12:24 +1000

    adds a MatchParams type to the repo to be used in match-related queries

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,3 +1,12 @@
 export interface LeaderboardPageProps {
     entity: string;
 }
+
+export interface MatchParams {
+    location: string;
+    matchTime: string;
+    finalOpponentScore: number;
+    finalPlayerScore: number;
+    opponentId: number;
+    playerId: number;
+}
